ca20a334bf5d8346bb7a4201b00489cb043799fb
anap-arejano <aparejano@gmail.com>
2022-11-30 12:20:09 -0300

    New components

--- binary_to_bcd.v
+++ binary_to_bcd.v
@@ -1,44 +1,28 @@
-/* Converte  um número binário de 8 bits em 
-um número BCD , formado por unidades (UNI), dezenas (DEZ)
-e centenas (CENT)
-entrada: 
-saída: 
- */
-module binary_to_BCD(A,UNI,DEZ,CENT);
-/* .
-Com 8 bits, podemos ter até o número 255. 
-Assim, UNI e DEZ variam de 0 a 9, utilizando 4 bits
-e CENT varia de 0 a 2, utilizando 2 bits.
-*/
-
-
-input [7:0] A; //Declara a entrada A de 8 bits
-output [3:0] UNI, DEZ; // Declara as saídas UNI e DEZ de 4 bits
-output [1:0] CENT; // Declara a saída CENT de 2 bits
-
-// Declarando fios de 4 bits
-wire [3:0] c1,c2,c3,c4,c5,c6,c7; 
-wire [3:0] d1,d2,d3,d4,d5,d6,d7;
-
-
-assign d1 = {1'b0,A[7:5]};  
-assign d2 = {c1[2:0],A[4]};
-assign d3 = {c2[2:0],A[3]};
-assign d4 = {c3[2:0],A[2]};
-assign d5 = {c4[2:0],A[1]};
-assign d6 = {1'b0,c1[3],c2[3],c3[3]};
-assign d7 = {c6[2:0],c4[3]};
-
-// Corrigindo com o modulo add3
-add3 m1(d1,c1);
-add3 m2(d2,c2);
-add3 m3(d3,c3);
-add3 m4(d4,c4);
-add3 m5(d5,c5);
-add3 m6(d6,c6);
-add3 m7(d7,c7);
-
-assign UNI = {c5[2:0],A[0]};
-assign DEZ = {c7[2:0],c5[3]};
-assign CENT = {c6[3],c7[3]};
+module Binary_to_BCD(A, Units, Tens, Hundreds);
+	input [7:0] A;
+	output [3:0] Units, Tens;
+	output [1:0] Hundreds;
+	
+	wire [3:0] c1, c2, c3, c4, c5, c6, c7;
+	wire [3:0] d1, d2, d3, d4, d5, d6, d7;
+	
+	assign d1 = {1'b0, A[7:5]};
+	assign d2 = {c1[2:0], A[4]};
+	assign d3 = {c2[2:0], A[3]};
+	assign d4 = {c3[2:0], A[2]};
+	assign d5 = {c4[2:0], A[1]};
+	assign d6 = {1'b0, c1[3], c2[3], c3[3]};
+	assign d7 = {c6[2:0], c4[3]};
+	
+	Add3 a1(d1,c1);
+	Add3 a2(d2,c2);
+	Add3 a3(d3,c3);
+	Add3 a4(d4,c4);
+	Add3 a5(d5,c5);
+	Add3 a6(d6,c6);
+	Add3 a7(d7,c7);
+	
+	assign Units = {c5[2:0], A[0]};
+	assign Tens = {c7[2:0], c5[3]};
+	assign Hundreds = {c6[3], c7[3]};
 endmodule
--- overflow_1b.v
+++
@@ -1,7 +0,0 @@
-module overflow_1b(cin, cout, sum, set, overflow);
-	input cin, cout, sum;
-	output set, overflow;
-
-	xor xo1 (overflow, cin, cout);
-	xor xo2 (set, sum, overflow);
-endmodule
